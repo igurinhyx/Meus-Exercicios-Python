291710f2d3e0acc370c207b7d6065a6a7ac60060
Igor Cavalcanti <igurohub@gmail.com>
2026-08-20 16:28:29 -0300

    Adicionando mais exercícios

---
+++ ex032.py
@@ -0,0 +1,6 @@
+ano = int(input('Qual a quantidade de dias desse ano? '))
+if ano >= 366:
+    print('Esse ano, é um ano bissexto!')
+else:
+    print('Esse ano, NÃO é um ano bissexto!')
+
---
+++ ex033.py
@@ -0,0 +1,3 @@
+n1 = float(input('Digite um número qualquer: '))
+n2 = float(input('Agora outro: '))
+n3 = float(input('E outro: '))
