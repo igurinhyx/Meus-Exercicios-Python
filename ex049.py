4dcc93842744af6520a299f43e2756963784cb5a
Igor Cavalcanti <igurohub@gmail.com>
2026-08-24 22:22:38 -0300

    Corrigindo o ex049
    Finalmente entendi essa estrutura de repetição

--- ex049.py
+++ ex049.py
@@ -1,6 +1,6 @@
-numero = int(input('Digite o número da tabuada escolhida: '))
-multiplicacao = 0
-for multiplicacao in range (0, 11 * numero):
-        multiplicacao = multiplicacao * numero
-        print ('A tabuada do {} é {}'.format(numero, multiplicacao))
-
+print('Está com dificuldade na tabuada?')
+n1 = int(input('Digita um número aí: '))
+print('-'*12)
+for c in range (0, 11):
+        print('{} x {} = {}'.format(n1,c,n1*c))
+print('-'*12)
