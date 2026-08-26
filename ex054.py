d600c8d7968775900338833d4f9dc84769aa122c
Igor Cavalcanti <igurohub@gmail.com>
2026-08-26 11:10:01 -0300

    ex054 e 055
    Adicionando exercicios e corrigindo o 054

--- ex054.py
+++ ex054.py
@@ -1,2 +1,16 @@
+import datetime
+contagem = 0
+hoje = datetime.date.today().year
+totalmaior = 0
+totalmenor = 0
 for c in range (1, 8):
-    ano = int(input('Digite o ano de nascimento: '))
+    contagem +=1
+    ano = int(input('Digite o ano de nascimento da {} pessoa: '.format(contagem,)))
+    if (hoje - ano)<= 18:
+        totalmenor += 1
+    else:
+        totalmaior += 1
+print('Temos {} em maioridade'.format(totalmaior))
+print('Temos {} em menorridade'.format(totalmenor))
+
+
