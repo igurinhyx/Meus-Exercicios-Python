26da10e8b1cd2f0810c64141e7fe81d7ae610cad
Igor Cavalcanti <igurohub@gmail.com>
2026-08-29 20:34:36 -0300

    Corrigindo o ex057

--- ex057.py
+++ ex057.py
@@ -1,6 +1,13 @@
-homem = 'M'
-mulher = 'F'
 sexo = ''
-
-while genero not
+genero = ''
+while genero != 'M' and genero != 'F':
+    genero = str(input('''Qual seu sexo? [M / F]
+    Digite aqui: ''')).upper().strip()
+    if genero == 'M':
+        genero = 'Masculino'
+        print('Anotado: {}'.format(genero))
+    else:
+        genero = 'Feminino'
+        print('Anotado: {}'.format(genero))
+print('FIM')
 
