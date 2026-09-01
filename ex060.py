6145cd8c33b720db0dad6cd98a2959e7abab00b7
Igor Cavalcanti <igurohub@gmail.com>
2026-09-01 18:25:50 -0300

    adicionando ex060

---
+++ ex060.py
@@ -0,0 +1,9 @@
+numero = int(input('Digite um numero para ver seu fatorial: '))
+c = numero
+f = 1
+while c > 0:
+    print('{}'.format(c), end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print('{}'.format(f))
