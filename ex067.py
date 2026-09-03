847ef277094a2b766f095552dcc7ff280510c885
Igor Cavalcanti <igurohub@gmail.com>
2026-09-03 11:04:01 -0300

    Corrigindo ex067 e adicionando ex071

--- ex067.py
+++ ex067.py
@@ -4,16 +4,7 @@ while True:
     if num < 0:
         break
     print('_'*20)
-    print(f'''
-{num} x 1 = {num*1}
-{num} x 2 = {num*2}
-{num} x 3 = {num*3}
-{num} x 4 = {num*4}
-{num} x 5 = {num*5}
-{num} x 6 = {num*6}
-{num} x 7 = {num*7}
-{num} x 8 = {num*8}
-{num} x 9 = {num*9}
-{num} x 10 = {num*10}''')
+    for c in range (1, 11):
+        print(f'{num} x {c} = {num*c}')
     print('_' * 20)
 print('FIM')
---
+++ ex071.py
@@ -0,0 +1,1 @@
+valor = int(input('Qual o valor, a ser sacado? Digite aqui: '))
