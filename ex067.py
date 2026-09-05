ee16db3fd7e8b3b4fa5bf66b15df9d1790d9a2b8
Igor Cavalcanti <igurohub@gmail.com>
2026-09-04 22:09:37 -0300

    políndo ex067

--- ex067.py
+++ ex067.py
@@ -1,10 +1,10 @@
-print('Quer ver a tabuada de qual valor?')
+print('\033[1;32mQuer ver a tabuada de qual valor?\033[m')
 while True:
-    num = int(input('\033[1mDigite aqui e se quiser PARAR [ - (n) ]:\033[m '))
+    num = int(input('\033[1mDigite aqui e se quiser\033[m \033[1;31mPARAR [ - (n) ]\033[m: '))
     if num < 0:
         break
-    print('_'*20)
+    print('\033[1;36m_\033[m'*20)
     for c in range (1, 11):
-        print(f'{num} x {c} = {num*c}')
-    print('_' * 20)
+        print(f'\033[1;33m{num}\033[m \033[1;31mx\033[m \033[1;33m{c}\033[m = \033[1;32m{num*c}\033[m')
+    print('\033[1;36m_\033[m'* 20)
 print('FIM')
