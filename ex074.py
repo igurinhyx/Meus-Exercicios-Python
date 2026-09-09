5ae3c38e84ad9d53aebd5ac76cc77a8d13e2da56
Igor Cavalcanti <igurohub@gmail.com>
2026-09-09 13:43:14 -0300

    Corrigindo os exercicios
    Estou chocado com as coisas que eu descobri hoje.

--- ex074.py
+++ ex074.py
@@ -1,22 +1,10 @@
 import random
-maior = 0
-menor = 0
-num = 0
+
 
 lista = random.randint(0, 10), random.randint(0, 10), random.randint(0, 10), random.randint(0, 10), random.randint(0, 10)
-
-for cont in range (len(lista)):
-    num = (lista[cont])
-    cont += 1
-    if cont == 1:
-        maior = menor = num
-    else:
-        if num > maior:
-            maior = num
-        if num < menor:
-            menor = num
-
-print(f'\033[1mOs numeros sorteados são:\033[m \033[1;32m{lista}\033[m')
-print(f'\033[1mO maior numero é:\033[m \033[1;36m{maior}\033[m')
-print(f'\033[1mO menor numero é:\033[m \033[1;35m{menor}\033[m')
+print(f'\033[1mOs numeros sorteados são:\033[m ', end=' ')
+for c in lista:
+    print(f'\033[1;32m{c}\033[m', end=' ')
+print(f'\n\033[1mO maior numero é:\033[m \033[1;36m{max(lista)}\033[m')
+print(f'\033[1mO menor numero é:\033[m \033[1;35m{min(lista)}\033[m')
 
--- ex075.py
+++ ex075.py
@@ -1,13 +1,8 @@
-nove = 0
-n1 = int(input('Digite o 1 numero: '))
-n2 = int(input('Digite o 2 numero: '))
-n3 = int(input('Digite o 3 numero: '))
-n4 = int(input('Digite o 4 numero: '))
-lista = n1, n2, n3, n4
+num = (int(input('Digite um numero: ')),
+int(input('Digite outro numero: ')),
+int(input('Digite mais um numero: ')),
+int(input('Digite o ultimo numero: ')))
+print(num)
+print(num.count(9))
 
-for cont in range (len(lista)):
-    cont += 1
-    nove = lista.count(9)
 
-print(lista)
-print(nove)
