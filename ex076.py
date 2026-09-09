27abf8977d59a36faee8df2d9b2a45363532c405
Igor Cavalcanti <igurohub@gmail.com>
2026-09-09 14:29:23 -0300

    Corrigindo ex076

--- ex076.py
+++ ex076.py
@@ -5,5 +5,13 @@ lista = (
     'Queijo', 4.50,
     'Peixe', 10.50,
 )
-for cont in range (0, 6):
-    print(lista[cont])
+print('\033[1;34m-=-\033[m'*10)
+print('LISTA DE PRODUTOS: ')
+print('\033[1;34m-=-\033[m'*10)
+
+for produto in range (0, len(lista)):
+    if produto % 2 == 0:
+        print(f'\033[1m{lista[produto]:<10}\033[m',end= '')
+    if produto % 2 != 0:
+        print(f'\033[1;33m{lista[produto]:>7.2f}\033[m \033[1;32mR$\033[m')
+
