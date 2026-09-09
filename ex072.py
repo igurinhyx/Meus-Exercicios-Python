aa49d145c8b672330b2336ea39f8f97698c48a50
Igor Cavalcanti <igurohub@gmail.com>
2026-09-09 11:02:26 -0300

    Corrigindo ex072

--- ex072.py
+++ ex072.py
@@ -1,11 +1,17 @@
 lista_num = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
 num_extenso = ''
 while True:
-        num = int(input('\033[1mDigite um numero\033[m \033[1;33mentre 0 e 20:\033[m '))
-        if 0 <= num <= 20:
+    num = int(input('\033[1mDigite um numero\033[m \033[1;33mentre 0 e 20:\033[m '))
+    if 0 <= num <= 20:
+        num_extenso = lista_num[num]
+        print(f'\033[1mVoce digitou\033[m \033[1;32m{num_extenso}\033[m')
+    else:
+        print('Você \033[1;31mnão digitou um número válido.\033[m \033[1;32mTente novamente!\033[m')
+    if 0 <= num <= 20:
+        escolha = str(input('\033[1mDigite\033[m \033[1;31mF para finalizar\033[m \033[1;36mou\033[m \033[1;32mS para continuar\033[m: ')).strip().upper()[0]
+        if escolha == 'F':
             break
-        print('Você \033[1;31mnão digitou um número válido.\033[m \033[1;33mTente novamente!\033[m')
 
-print(f'\033[1mVocê digitou:\033[m \033[1;32m{lista_num[num]}\033[m')
 
+print('\033[1;32mFIM\033[m')
 
