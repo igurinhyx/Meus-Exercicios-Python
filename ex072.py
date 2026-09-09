685e0ae1154f6adb7f3524e2f73b203396acb0e9
Igor Cavalcanti <igurohub@gmail.com>
2026-09-09 12:21:55 -0300

    ex072

--- ex072.py
+++ ex072.py
@@ -1,4 +1,5 @@
 lista_num = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+
 num_extenso = ''
 while True:
     num = int(input('\033[1mDigite um numero\033[m \033[1;33mentre 0 e 20:\033[m '))
