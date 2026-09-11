7a9bc2f0d87ce0ad3e45dd4943b2d9e7a79bb0d8
Igor Cavalcanti <igurohub@gmail.com>
2026-09-11 14:10:02 -0300

    Corrigindo ex078

--- ex078.py
+++ ex078.py
@@ -1,24 +1,20 @@
 lista = []
 
-maior = 0
-menor = 0
-pos_maior = 0
-pos_menor = 0
-for c in range(0, 5):
-    lista.append(int(input('Digite um valor: ')))
+for cont in range(0, 5):
+    lista.append(int(input(f'Digite um valor para a \033[1;32mposição {cont}:\033[m ')))
 
-for c, l in enumerate(lista):
-    if max(lista) == l:
-        maior = l
-        pos_maior = lista.index(maior)
-    if min(lista) == l:
-        menor = l
-        pos_menor = lista.index(menor)
+print(f'A sua lista deu: {lista}')
+print(f'O \033[1;33mmaior valor é:\033[m \033[1;31m{max(lista)}\033[m ')
+print('E ele \033[1mapareceu nas posições:\033[m ', end= ' ')
+for pos, valor in enumerate(lista):
+    if valor == max(lista):
+        print(f'\033[1;32m{pos}\033[m...', end= ' ')
 
-    print(f'Na posição {c + 1}, temos {l}')
+print()
+print(f'O \033[1;34mmenor valor\033[m é: \033[1;36m{min(lista)}\033[m')
+print('E ele \033[1mapareceu nas posições: \033[m', end= ' ')
+for pos, valor in enumerate(lista):
+    if valor == min(lista):
+        print(f'\033[1;32m{pos}\033[m...', end= ' ')
 
-print(maior)
-print(menor)
-print(pos_maior)
-print(pos_menor)
 
