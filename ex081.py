69ece6de2ecab84b36c9afb05a683613c3ec6539
Igor Cavalcanti <igurohub@gmail.com>
2026-09-11 14:53:33 -0300

    Corrigindo ex081

--- ex081.py
+++ ex081.py
@@ -1,13 +1,13 @@
 cont = 0
 lista = []
 while True:
-    valor = int(input('Digite um numero: '))
-    lista.append(valor)
+    lista.append(int(input('Digite um numero: ')))
     cont += 1
     escolha = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
 
     if escolha == 'N':
         break
+
 lista.sort(reverse=True)
 if 5 in lista:
     print(f'O cinco foi encontrado {lista.count(5)} vezes')
