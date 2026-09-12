4b92e3c306a255ba4b7c25030fd0c09e7e1636da
Igor Cavalcanti <igurohub@gmail.com>
2026-09-12 20:36:54 -0300

    Correção ex084

--- ex084.py
+++ ex084.py
@@ -1,18 +1,31 @@
-pessoas = list()
-nome = list()
-qtd = 0
-menor = 0
-maior = 0
+temporario = []
+principal = []
+maior = menor = 0
 while True:
-    nome.append(str(input('Nome: ')))
-    nome.append(float(input('Peso: ')))
-    pessoas.append(nome[:])
-    nome.clear()
-    qtd += 1
 
+    temporario.append(str(input('Digite o nome: ')))
+    temporario.append(float(input('Digite o peso: ')))
+    if len(principal) == 0:
+        maior = menor = temporario[1]
+    else:
+        if temporario[1] > maior:
+            maior = temporario[1]
+        if temporario[1] < menor:
+            menor = temporario[1]
+    principal.append(temporario[:])
+    temporario.clear()
     escolha = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
-    if escolha == 'N':
+    if escolha in 'N':
         break
-print(f'A quantidade de pessoas cadastradas foram: {qtd}')
-
-
+print(f'Os dados foram: {principal}')
+print(f'A quantidade de dados salvos foram: {len(principal)}')
+print()
+print(f'As pessoas com os maiores pesos foram: ', end= ' ')
+for pessoa in principal:
+    if pessoa[1] == maior:
+        print(f'{pessoa[0]} com {maior} |', end=' ')
+print()
+print(f'As pessoas com os menores pesos foram: ', end= ' ')
+for pessoa in principal:
+    if pessoa[1] == menor:
+        print(f'{pessoa[0]} com {menor} |', end=' ')
