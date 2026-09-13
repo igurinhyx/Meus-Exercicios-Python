0234c0a9d49c4da5e5ad4777e1a6de20f5ddcf78
Igor Cavalcanti <igurohub@gmail.com>
2026-09-13 20:26:30 -0300

    Terminando correção ex088

--- ex088.py
+++ ex088.py
@@ -1,14 +1,30 @@
 from random import randint
-cont = 0
+from time import sleep
 lista = list()
+jogos = list()
+total = 1
 
+print('=-='*20)
+print('MEGA SENA')
+print('=-='*20)
 quantidade = int(input('Digite a quantidade de jogos que você precisa: '))
-while True:
-    num = randint(1, 60)
-    if num not in lista:
-        lista.append(num)
-        cont += 1
-    if cont >= 6:
-        break
-print(lista)
+while total <= quantidade:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    total += 1
+
+for l, c in enumerate(jogos):
+    print(f'Jogo {l+1}: {c}')
+    sleep(1)
+
+
 
