b901d692d4c3432349a9b318a26b533422beb7da
Igor Cavalcanti <igurohub@gmail.com>
2026-09-13 20:59:55 -0300

    Ultima correção ex089

--- ex089.py
+++ ex089.py
@@ -1,16 +1,28 @@
+ficha = list()
 
-lista = list()
-medias = list()
-nome = list()
+
 while True:
-    nome.append(str(input('Nome: ')))
-    media1 = medias.append(float(input('Media 1: ')))
-    media2 = medias.append(float(input('Media 2: ')))
-    lista.append(nome[:])
-    lista.append(medias[:])
-    nome.clear()
-    medias.clear()
-    escolha = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    media = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], media])
+    escolha = str(input('Quer continuar? [S/N]:  ')).strip().upper()[0]
     if escolha == 'N':
         break
-print(lista)
+
+print(f'\033[1m{"N°.":<4} {"Nome":<10} {"Media":>8}\033[m')
+print('\033[1m==\033[m'*25)
+for i, a in enumerate(ficha):
+    print(f'{i:<4} {a[0]:<10} {a[2]:>8}')
+print('\033[1m==\033[m'*25)
+
+while True:
+    escolha2 = int(input('Quer ver as notas de qual aluno? [999 PARAR]: '))
+    if escolha2 == 999:
+        print('FINALIZANDO...')
+        break
+    if escolha2 <= len(ficha) - 1:
+        print(f'Notas de {ficha[escolha2][0]}: {ficha[escolha2][1]}')
+
+
