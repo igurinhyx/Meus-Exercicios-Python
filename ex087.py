9d1499e91533d5a5518e581e232aaff8e45e83fe
Igor Cavalcanti <igurohub@gmail.com>
2026-09-13 20:00:30 -0300

    Corrigindo ex087

--- ex087.py
+++ ex087.py
@@ -1,46 +1,21 @@
-lista = list()
-num = list()
-pares = list()
-soma = 0
-soma_par = 0
-for c in range(0, 9):
-        valor = num.append(int(input(f'Digite o valor para [{c},{c}]: ')))
-        lista.append(num[:])
-        if num[0] % 2 == 0:
-            pares.append(num[:])
-        if c == 3:
-            soma += valor
-        if c == 6:
-            soma += valor
-        if c == 9:
-            soma += valor
-
-        num.clear()
-
-print(lista[0:3])
-print(lista[3:6])
-print(lista[6:])
-print(soma)
-
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+spares = maior = scolunas = 0
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite o valor para [{l},{c}]: '))
 print()
-for c in pares:
-    soma_par = pares [c:c] + pares [c:c]
-    c += 1
-print(pares)
-print(max(pares))
-print(soma_par)
 
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end= ' ')
+        if matriz[l][c] % 2 == 0:
+            spares += matriz[l][c]
 
+    print()
+print(spares)
+print(matriz[0][2]+matriz[1][2]+matriz[2][2])
 
-
-
-
-
-
-#for n in lista[:][:]:
-    #if n % 2 == 0:
-        #print(n)
-
-#for n in lista[:][:]:
-    #if n % 2 != 0:
-        #print(n)
+for l, matriz[1][] in range(0, 3):
+    if l > matriz[1][l]:
+        maior = l
+print(maior)
