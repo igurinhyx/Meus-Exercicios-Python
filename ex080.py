0fea45a35e6c7045a00ccd81d92afebf73239e50
Igor Cavalcanti <igurohub@gmail.com>
2026-09-13 23:14:11 -0300

    atualizando ex080

--- ex080.py
+++ ex080.py
@@ -1,7 +1,7 @@
 lista = []
 
 for c in range(0, 5):
-    valor = (int(input('Digite um valor: ')))
+    valor = (int(input(f'\033[1mDigite o\033[m \033[1;33m{c+1}°\033[m \033[1mvalor: \033[m')))
     if c == 0 or valor > lista[-1]:
         lista.append(valor)
     else:
@@ -12,4 +12,6 @@ for c in range(0, 5):
                 break
         pos += 1
 
-print(lista)
+print()
+print('\033[1mSua lista é:\033[m ')
+print(f'\033[1;32m{lista}\033[m')
