0f7d6f4b3abc38a8d7ded631f61161faa8bb0c31
Igor Cavalcanti <igurohub@gmail.com>
2026-09-16 20:47:42 -0300

    Corrigindo ex091

--- ex091.py
+++ ex091.py
@@ -1,22 +1,13 @@
 from random import randint
 from time import sleep
 
-lista = dict()
-maior = menor = 0
-for c in range(1, 5):
-    jogador = randint(1, 6)
-    print(f'O JOGADOR N°: {c}, tirou: {jogador}')
-    sleep(1)
-    lista[c] = jogador
+jogo = {
+    'Jogador1': randint(1, 6),
+    'Jogador2': randint(1, 6),
+    'Jogador3': randint(1, 6),
+    'Jogador4': randint(1, 6)
+}
 print()
-for c in lista:
-    for l in lista:
-        if c == 0:
-            maior = menor = lista[c][l]
-        else:
-            if lista[c][l] > maior:
-                maior = lista[c][l]
-            if lista[c][l] < menor:
-                menor = lista[c][l]
-print(maior)
-print(menor)
+for k, v in jogo.items():
+    print(f'O \033[1;35m{k}\033[m tirou: \033[1;34m[\033[m\033[1;36m{v}\033[m\033[1;34m]\033[m')
+    sleep(1)
