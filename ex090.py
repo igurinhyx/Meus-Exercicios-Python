04340b3e2e7ea6ba5b981b4370432f36b2aac085
Igor Cavalcanti <igurohub@gmail.com>
2026-09-16 20:30:12 -0300

    Mudando uma parte do ex090

--- ex090.py
+++ ex090.py
@@ -6,13 +6,13 @@ aluno = dict()
 aluno['nome'] = str(input('\033[1;35mNome\033[m do \033[1;36maluno(a):\033[m '))
 aluno['media'] = float(input(f'\033[1;35mMedia\033[m do(a) \033[1;36m{aluno["nome"]}\033[m: '))
 
-if aluno['media'] < 5:
+if aluno['media'] < 7:
     situacao = 'Reprovado(a)'
 else:
     situacao = 'Aprovado(a)'
 
 print(f'Nome: \033[1;34m{aluno["nome"]}\033[m')
-if aluno['media'] < 5:
+if aluno['media'] < 7:
     print(f'Media: \033[1;31m{aluno["media"]}\033[m')
 else:
     print(f'Media: \033[1;32m{aluno["media"]}\033[m')
