ff37ebd553e17ee0483d8f6a5dbbf6a195318eed
Igor Cavalcanti <igurohub@gmail.com>
2026-09-17 17:08:14 -0300

    Corrigido o ex092

--- ex092.py
+++ ex092.py
@@ -1,20 +1,19 @@
 from datetime import datetime
 ficha = dict()
 
-while True:
-    ficha['Nome'] = str(input('Nome: '))
-    nascimento = int(input('Digite seu ano de nascimento: '))
-    ficha['idade'] = datetime.now().year - nascimento
-    ficha['CTPS'] = int(input('Carteira de trabalho, \033[1;33m[\033[m \033[1;31m0 não tem\033[1m \033[1;33m]\033[m: '))
-    if ficha['CTPS'] == 0:
-        break
-    else:
-        ficha['Ano de contratacao'] = int(input('Ano de contratação: '))
-        ficha['Salario'] = float(input('Salário: '))
-        print()
-        escolha = str(input('Digite [F] para finalizar: ')).strip().upper()[0]
-        if escolha == 'F':
-            break
 
+ficha['Nome'] = str(input('Nome: '))
+nascimento = int(input('Ano de nascimento: '))
+ficha['idade'] = datetime.now().year - nascimento
+ficha['CTPS'] = int(input('Carteira de trabalho, \033[1;33m[\033[m \033[1;31m0 não tem\033[1m \033[1;33m]\033[m: '))
+if ficha['CTPS'] != 0:
+    ficha['Contratação'] = int(input('Ano de contratação: '))
+    ficha['Salario'] = float(input('Salário: '))
+    ficha['Aposentadoria'] = ficha['idade'] + ((ficha['Contratação'] +35) - datetime.now().year)
+    print()
+print('=='*20)
+for k, v in ficha.items():
+    print(f'{k}: {v}')
+print('=='*20)
 
 
