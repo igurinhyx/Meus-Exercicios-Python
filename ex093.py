e88ecb55aa8c66ac0113d32625c7d2e1e47d73cb
Igor Cavalcanti <igurohub@gmail.com>
2026-09-17 18:31:01 -0300

    Correção completa do ex093

--- ex093.py
+++ ex093.py
@@ -5,5 +5,15 @@ total = int(input(f'Quantidades de partidas que o {jogador['Nome']}, jogou: '))
 
 for c in range(0, total):
     partidas.append(int(input(f'Quantos gols o {jogador['Nome']} fez na {c+1}° partida: ')))
+jogador['Gols'] = partidas[:]
+jogador['Total'] = sum(partidas)
+print()
 print(jogador)
-print(partidas)
+print()
+for k, v in jogador.items():
+    print(f'{k}: {v}')
+print()
+print(f'O {jogador["Nome"]} jogou: {len(partidas)} partidas')
+print()
+for i, v in enumerate(jogador['Gols']):
+    print(f'Na {i + 1}° partida, o {jogador["Nome"]} marcou {v} gols.')
