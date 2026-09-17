bdda7196c57bbe7ff1ca4aa6cc28aa7656333eb2
Igor Cavalcanti <igurohub@gmail.com>
2026-09-17 17:33:45 -0300

    Organizando o ex093 p/ correção

--- ex093.py
+++ ex093.py
@@ -1,16 +1,9 @@
-dicionario = dict
-ficha = dict()
-partidas = dict()
-partidas_lista = []
-soma = 0
-ficha['jogador'] = str(input('Nome (jogador): '))
-ficha['partidas'] = int(input('Partidas jogadas: '))
-for c in range(ficha['partidas']):
-    partidas['gols'] = int(input(f'Digite o valor de gols da {c+1}° partida: '))
-    partidas_lista.append(partidas['gols'])
-    soma += partidas['gols']
-dicionario.copy(ficha)
+jogador = dict()
+partidas = list()
+jogador['Nome'] = str(input('Nome (jogador): '))
+total = int(input(f'Quantidades de partidas que o {jogador['Nome']}, jogou: '))
 
-
-print(soma)
-print(dicionario)
+for c in range(0, total):
+    partidas.append(int(input(f'Quantos gols o {jogador['Nome']} fez na {c+1}° partida: ')))
+print(jogador)
+print(partidas)
