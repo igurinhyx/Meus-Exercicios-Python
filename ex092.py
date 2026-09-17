07b4db85ee0f376c3d1159bdb4b7e009f38e6340
Igor Cavalcanti <igurohub@gmail.com>
2026-09-17 16:50:26 -0300

    Começando as correções do ex092

--- ex092.py
+++ ex092.py
@@ -16,13 +16,17 @@ while True:
         escolha = str(input('Digite [F] para finalizar: ')).strip().upper()[0]
         if escolha == 'F':
             break
-
-print(f'Nome: {ficha["Nome"]}')
-print(f'Idade: {anoatual - ficha["Ano de nascimento"]}')
-print(f'CTPS: {ficha["CTPS"]}')
-print(f'Ano de contratação: {ficha["Ano de contratacao"]}')
-print(f'Salário: {ficha["Salario"]}')
-print(f'Anos de serviço: {- anoatual - ficha["Ano de contratacao"]}')
+if ficha['CTPS'] == 0:
+    print(f'Nome: {ficha["Nome"]}')
+    print(f'Idade: {anoatual - ficha["Ano de nascimento"]}')
+    print(f'CTPS: Não possui.')
+else:
+    print(f'Nome: {ficha["Nome"]}')
+    print(f'Idade: {anoatual - ficha["Ano de nascimento"]}')
+    print(f'CTPS: {ficha["CTPS"]}')
+    print(f'Ano de contratação: {ficha["Ano de contratacao"]}')
+    print(f'Salário: {ficha["Salario"]}')
+    print(f'Anos de serviço: {anoatual - ficha["Ano de contratacao"]}')
 
 
 
