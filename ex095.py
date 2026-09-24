37790f93e7d73667103c17c207af6ff0f6f88dca
Igor Cavalcanti <igurohub@gmail.com>
2026-09-24 20:24:22 -0300

    Atualizando ex095

--- ex095.py
+++ ex095.py
@@ -1,11 +1,12 @@
-ficha = dict()
+ficha
+jogador = dict()
 gols = list()
 
 while True:
-    jogador = str(input('Nome do jogador: '))
-    total_pt = int(input(f'Quantidade de partidas do {jogador}: '))
+    ficha['Nome'] = str(input('Nome do jogador: '))
+    ficha['Partidas'] = int(input(f'Quantidade de partidas do {ficha["Nome"]}: '))
 
-    for c in range(0, total_pt):
+    for c in range(0, ficha["Partidas"]):
         gol = int(input(f'Quantidade de gols na partida {c+1}: '))
         gols.append(gol)
 
@@ -15,8 +16,9 @@ while True:
             print('Errado! Escreva S ou N')
         else:
             break
+    ficha['Gols'] = gols[:]
     gols.clear()
     if escolha == 'N':
         break
 
-
+print(ficha)
